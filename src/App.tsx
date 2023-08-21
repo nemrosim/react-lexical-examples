f0e3649531a1396b7bd115565ea356e4201beb5c
artemdiashkin <nemrosim1988@gmail.com>
2023-08-21 20:06:03 +0300

    add lexical

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import './App.css';
+import React, {useMemo} from 'react';
+import {InitialConfigType, LexicalComposer} from '@lexical/react/LexicalComposer';
+import {PlainTextPlugin} from "@lexical/react/LexicalPlainTextPlugin";
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
 
-function App() {
+export const App: React.FC = () => {
+
+    const CustomContent = useMemo(() => {
+        return (
+            <ContentEditable style={{
+                position: 'relative',
+                borderColor: 'rgba(255,211,2,0.68)',
+                border: '2px solid red',
+                borderRadius: '5px',
+                maxWidth: '100%',
+                padding: '10px'
+            }}/>
+        )
+    }, []);
+
+    const CustomPlaceholder = useMemo(() => {
+        return (
+            <div style={{
+                position: 'absolute', top: 30, left: 30,
+            }}>
+                Enter some text...
+            </div>
+        )
+    }, []);
+
+    const lexicalConfig: InitialConfigType = {
+        namespace: 'My Rich Text Editor',
+        onError: (e) => {
+            console.log('ERROR:', e)
+        }
+    }
+
     return (
-        <div>
-
+        <div style={{padding: '20px'}}>
+            <LexicalComposer initialConfig={lexicalConfig}>
+                <PlainTextPlugin
+                    contentEditable={CustomContent}
+                    placeholder={CustomPlaceholder}
+                    ErrorBoundary={LexicalErrorBoundary}
+                />
+            </LexicalComposer>
         </div>
     );
 }
-
-export default App;
